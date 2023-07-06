5ac633d0d30f3592c77ba9971b2194a2e5ae4ad7
Sanjar17 <sanjarsaidov173@gmail.com>
2023-07-06 22:13:21 +0500

    wrong viewset

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,14 +2,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectViewSet, AboutMeViewSet, SkillViewSet, ContactViewSet, FrontendViewSet, BackendViewSet
+from .views import ProjectViewSet, AboutMeViewSet, SkillViewSet, ContactViewSet, FrontendViewSet, BackendViewSet, FrontendSkillViewSet, BackendSkillViewSet
 
 router = DefaultRouter()
 router.register('project', ProjectViewSet, basename='project')
 router.register('about', AboutMeViewSet, basename='about')
 router.register('skill', SkillViewSet, basename='skills')
-router.register('frontend-skill', FrontendViewSet, basename='frontend-skills')
-router.register('backend-skill', BackendViewSet, basename='backend-skills')
+router.register('frontend-skill', FrontendSkillViewSet, basename='frontend-skills')
+router.register('backend-skill', BackendSkillViewSet, basename='backend-skills')
 router.register('contact', ContactViewSet, basename='contacts')
 router.register('frontend', FrontendViewSet, basename='frontend')
 router.register('backend', BackendViewSet, basename='backend')
