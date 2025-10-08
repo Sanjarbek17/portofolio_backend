edd6af4a7d7164f548ce541a4fd775985f75930b
Sanjarbek17 <sanjarsaidov173@gmail.com>
2025-10-08 11:39:02 +0500

    Set DEBUG to True for development environment

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+8e#j*5-jnj44ruogoa=(r)(mqzw)@-4e4ane%1ifqjs7ymd2e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["sanjarbek1718.pythonanywhere.com", "*"]
 
